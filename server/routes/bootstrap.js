674bed089f2abd17c5dc1ae5223c31442c23ceb8
ghostt <63957220+pseudodev3@users.noreply.github.com>
2026-09-18 15:49:46 +0100

    Keep normalized usernames server internal

--- server/routes/bootstrap.js
+++ server/routes/bootstrap.js
@@ -16,7 +16,7 @@ router.get('/', auth, async (req, res) => {
 
     const [user, notifications] = await Promise.all([
       User.findById(req.user.id)
-        .select('-password -resetPasswordToken -resetPasswordExpire -welcomeAuraGranted -lastDailyAuraBonusKey -authVersion')
+        .select('-password -resetPasswordToken -resetPasswordExpire -welcomeAuraGranted -lastDailyAuraBonusKey -authVersion -usernameNormalized')
         .lean(),
       Notification.find({ toUserId: req.user.id })
         .sort({ createdAt: -1 })
